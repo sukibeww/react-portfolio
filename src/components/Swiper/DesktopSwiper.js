8346483213d197d95f27549080ec2f3040b51cf2
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-11-04 15:17:14 +1100

    modularize DesktopSwiper and MobileSwiper

---
+++ src/components/Swiper/DesktopSwiper.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import Swiper from 'react-id-swiper';
+
+const SectionContainer = styled.div`
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 80vh;
+  object-fit: cover;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+`
+
+const SwiperWrapper = styled.div`
+  width: 100vw;
+`
+
+const DesktopSwiper = () => {
+  const params = {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    spaceBetween: 0
+  }
+  return(
+    <>
+      <script src="https://unpkg.com/react-id-swiper@2.3.2/lib/react-id-swiper.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"></link>
+      <SwiperWrapper>
+        <Swiper {...params}>
+          <div>
+            <SectionContainer>
+              <img src={require("../../images/swiperlanding.svg")} alt="landing"></img>
+            </SectionContainer>
+          </div>
+          <div>
+            <SectionContainer>
+              <img src={require("../../images/mobile-friendly-light.svg")} alt="mobile-landing" ></img>
+            </SectionContainer>
+          </div>
+          <div>
+            <a href="https://github.com/sukibeww">
+              <SectionContainer>
+                <img src={require("../../images/github-desktop.svg")} alt="github-landing"></img>
+              </SectionContainer>
+            </a>
+          </div>
+        </Swiper>
+      </SwiperWrapper>
+    </>
+  )
+}
+
+export default DesktopSwiper;
---
+++ src/components/Swiper/MobileSwiper.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import Swiper from 'react-id-swiper';
+
+const MobileSectionContainer = styled.div`
+  background-image: url(${require("../../images/mobile-landing.svg")});
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 80vh;
+  object-fit: cover;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+`
+
+const SwiperWrapper = styled.div`
+  width: 100vw;
+`
+
+
+const MobileSwiper = () =>{
+  const params = {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    spaceBetween: 0
+  }
+  return(
+    <>
+      <script src="https://unpkg.com/react-id-swiper@2.3.2/lib/react-id-swiper.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"></link>
+      <SwiperWrapper>
+        <Swiper {...params}>
+          <div>
+            <MobileSectionContainer>
+              <img src={require("../../images/mobile-landing.svg")} alt="landing"></img>
+            </MobileSectionContainer>
+          </div>
+          <div>
+            <MobileSectionContainer>
+              <img src={require("../../images/mobile-friendly-for-mobile.svg")} alt="landing"></img>
+            </MobileSectionContainer>
+          </div>
+          <div>
+            <a href="https://github.com/sukibeww">
+              <MobileSectionContainer>
+                <img src={require("../../images/github-for-mobile.svg")} alt="landing"></img>
+              </MobileSectionContainer>
+            </a>
+          </div>
+        </Swiper>
+      </SwiperWrapper>
+    </>
+  )
+}
+
+export default MobileSwiper;
