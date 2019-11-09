5bbb2ff38b003f66db8a234b2c2b33c7790ed376
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-11-09 18:38:13 +1100

    initialized mobile and tablet footer

---
+++ src/components/Footer/MobileFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileFooter = () => {
+
+  return(
+    <>
+    </>
+  )
+}
+
+export default MobileFooter;
---
+++ src/components/Footer/TabletFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TabletFooter = () => {
+
+  return(
+    <>
+    </>
+  )
+}
+
+export default TabletFooter;
